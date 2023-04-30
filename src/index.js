b2849b0d1d4252f331dc19519b6a3f9029996dde
Mohamed Elwakeel <mo7mdhassan7@gmail.com>
2023-05-01 01:44:25 +0300

    deleted unnecessary files and installed bootstrap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +11,16 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// Let’s start playing around with React.
+// Create a new React project.
+// Add bootstrap to your project.
+// Try to create a form using bootstrap.
+// In case you meet any problems, take a look at these readings about React..
+// Create a layout for the website.
+// And Be creative! 
+//  Before you start with react, you should follow these steps:
+// Install Node JS.
+// Install Visual Studio Code.
+// Create a folder and access it using the command line.
+// Type “ code .” to open the folder in vscode.
